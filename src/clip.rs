b4832eb5b44cb4111c71b5caea6315061e9a052e
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-05-20 22:28:14 +0300

    fix

--- src/clip.rs
+++ src/clip.rs
@@ -6,15 +6,13 @@ pub fn copy_to_clipboard(content: &str) -> anyhow::Result<()> {
         Err(e) => return Err(anyhow::anyhow!("ClipboardContext Error: {}", e)),
     };
 
-    match ctx.set_contents(content.to_owned()) {
-        Ok(ctx) => ctx,
-        Err(e) => return Err(anyhow::anyhow!("Set Contents Error: {}", e)),
-    };
+    if let Err(e) = ctx.set_contents(content.to_owned()) {
+        return Err(anyhow::anyhow!("Set Contents Error: {}", e));
+    }
 
-    match ctx.get_contents() {
-        Ok(ctx) => ctx,
-        Err(e) => return Err(anyhow::anyhow!("Get Contents Error: {}", e)),
-    };
+    if let Err(e) = ctx.get_contents() {
+        return Err(anyhow::anyhow!("Get Contents Error: {}", e));
+    }
 
     Ok(())
 }
