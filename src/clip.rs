6b1763300547258c73f59c1dbaaa48295ece7a5a
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-05-20 14:02:23 +0300

    copies to clipboard

--- src/clip.rs
+++ src/clip.rs
@@ -1,25 +1,10 @@
-use std::fs;
-use std::io::Read;
+use anyhow::Ok;
+use copypasta::{ClipboardContext, ClipboardProvider};
 
-pub fn read_directory_contents(dir: &str) -> anyhow::Result<String> {
-    let mut combined_content = String::new();
+pub fn copy_to_clipboard(content: &str) -> anyhow::Result<()> {
+    let mut ctx = ClipboardContext::new().unwrap();
+    ctx.set_contents(content.to_owned()).unwrap();
+    ctx.get_contents().unwrap();
 
-    for entry in fs::read_dir(dir)? {
-        let entry = entry?;
-        let path = entry.path();
-
-        if path.is_file() {
-            let file_name = path.file_name().unwrap().to_string_lossy();
-            combined_content.push_str(&format!("File: {}\n", file_name));
-
-            let mut file_content = String::new();
-            let mut file = fs::File::open(&path)?;
-            file.read_to_string(&mut file_content)?;
-
-            combined_content.push_str(&file_content);
-            combined_content.push('\n');
-        }
-    }
-
-    Ok(combined_content)
+    Ok(())
 }
--- src/run.rs
+++ src/run.rs
@@ -1,14 +1,13 @@
 use clap::Parser;
 
-use crate::{cli, fs::read_directory_contents};
+use crate::{cli, clip::copy_to_clipboard, fs::read_directory_contents};
 
 pub fn run() -> anyhow::Result<()> {
     let opts = cli::Opts::parse();
     let path = opts.path;
 
     let contents = read_directory_contents(&path)?;
-
-    println!("Contents: {}", &contents);
+    copy_to_clipboard(&contents)?;
 
     Ok(())
 }
