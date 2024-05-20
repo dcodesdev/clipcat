8e8433c9e53a5b1361d8399beddd5e0f733b8abf
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-05-20 22:26:52 +0300

    fixes

--- src/clip.rs
+++ src/clip.rs
@@ -1,10 +1,20 @@
-use anyhow::Ok;
 use copypasta::{ClipboardContext, ClipboardProvider};
 
 pub fn copy_to_clipboard(content: &str) -> anyhow::Result<()> {
-    let mut ctx = ClipboardContext::new().unwrap();
-    ctx.set_contents(content.to_owned()).unwrap();
-    ctx.get_contents().unwrap();
+    let mut ctx = match ClipboardContext::new() {
+        Ok(ctx) => ctx,
+        Err(e) => return Err(anyhow::anyhow!("ClipboardContext Error: {}", e)),
+    };
+
+    match ctx.set_contents(content.to_owned()) {
+        Ok(ctx) => ctx,
+        Err(e) => return Err(anyhow::anyhow!("Set Contents Error: {}", e)),
+    };
+
+    match ctx.get_contents() {
+        Ok(ctx) => ctx,
+        Err(e) => return Err(anyhow::anyhow!("Get Contents Error: {}", e)),
+    };
 
     Ok(())
 }
--- src/run.rs
+++ src/run.rs
@@ -1,15 +1,13 @@
 use clap::Parser;
 
-use crate::{cli, clip::copy_to_clipboard, fs::read_directory_contents, tiktoken::count_tokens};
+use crate::{cli, clip::copy_to_clipboard, fs::read_directory_contents};
 
 pub fn run() -> anyhow::Result<()> {
     let opts = cli::Opts::parse();
     let path = opts.path;
 
     let contents = read_directory_contents(&path)?;
-    let tokens = count_tokens(&contents)?;
     copy_to_clipboard(&contents)?;
 
-    println!("{} tokens", tokens);
     Ok(())
 }
