21349e3927204b1831126c14fe044b7d6ff989bc
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2024-05-24 18:58:57 +0300

    with tokens

--- src/run.rs
+++ src/run.rs
@@ -1,15 +1,21 @@
 use clap::Parser;
 use std::path::Path;
 
-use crate::{cli, clip::copy_to_clipboard, fs::read_directory_contents};
+use crate::{cli, clip::copy_to_clipboard, fs::read_directory_contents, tiktoken::count_tokens};
 
 pub fn run() -> anyhow::Result<()> {
     let opts = cli::Opts::parse();
-    let path = opts.path;
+    let (path, token) = (opts.path, opts.token);
 
     let path = Path::new(&path);
     let contents = read_directory_contents(path)?;
+
     copy_to_clipboard(&contents)?;
+
+    if token {
+        let tokens = count_tokens(&contents)?;
+        println!("{} GPT-4 tokens.", tokens);
+    }
 
     Ok(())
 }
